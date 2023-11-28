d5e0897fa616ba36729dd20a801730117d9d333f
Franco Contreras <franco.contreras@alumnos.uv.cl>
2023-11-27 23:02:31 -0300

    123

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,58 @@ import postRoutes from "./routes/posts.js";
 import animalRoutes from "./routes/animalRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
+import { createRequire } from "module";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const require = createRequire(import.meta.url);
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://dbfranco:franco123@cluster0.8bqroil.mongodb.net/?retryWrites=true&w=majority";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+const mongoose = require("mongoose");
+
+mongoose
+    .connect(
+        process.env.MONGO_URL
+    )
+    .then(()=> console.log("mongoDBConnection Successfull!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.get("/api/test", ()=>{
+        console.log("test is successfull")
+})
+// app.listen(5000,()=>{
+//         console.log("soy mongodb")
+// })
 
 app.use("/Backend/auth", authRoutes);
 app.use("/Backend/usuarios", usuariosRoutes);
@@ -21,5 +68,5 @@ app.get("/test", (req, res) => {
 });
 
 app.listen(8800, () => {
-    console.log("connected");
+     console.log("connected");
 });
